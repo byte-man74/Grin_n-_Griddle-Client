8086d648ac41d43b40cf9a93378b6bfd868c39f3
Shereef Habib <shereef.habib@icubefarmhq.com>
2024-03-09 11:11:01 +0100

    staging cta button

--- frontend/src/components/LandingPage/heroSection.tsx
+++ frontend/src/components/LandingPage/heroSection.tsx
@@ -1,16 +1,17 @@
 import React, { ReactNode } from "react";
+import { ArrowSvg } from "../../assets/svg";
 
 interface HeroTextAndCTAProps {
   children: ReactNode;
 }
 
 const HeroTextAndCTA: React.FC<HeroTextAndCTAProps> = ({ children }) => {
-  return <div className="w-[90%] min-h-10 mt-[60px] flex gap-[80px]">{children}</div>;
+  return <div className="w-[90%] min-h-10 mt-[60px] flex gap-[80px] justify-center">{children}</div>;
 };
 
 export const HeroText = () => {
   return (
-    <h1 className="text-black w-[54%] text-[65px] font-bold leading-normal">
+    <h1 className="text-black w-[750px] text-[65px] font-bold leading-normal">
       🌶️ Savor the Spice, Share the Smile! 😁
     </h1>
   );
@@ -18,12 +19,15 @@ export const HeroText = () => {
 
 export const HeroCTA = () => {
   return (
-    <div className="flex w-[48%] flex-col gap-2">
-      <p className="w-[90%] text-black mt-8">
+    <div className="flex w-[590px] flex-col gap-[50px]">
+      <p className="w-[90%] text-black mt-8 leading-normal">
         Spreading Joy, One Bite at a Time. Welcome to the heart and soul of
         delicious dining in Naija style! Dive into a world where flavor dances,
         and grins are the main course.
       </p>
+      <div className="flex items-center gap-4">
+        <ArrowSvg />
+      </div>
     </div>
   );
 };
---
+++ frontend/src/components/global/button.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Button = ({background, children, onClick, icon, width, height}) => {
+  return (
+    <div></div>
+  )
+}
