5b251f894f60c38e244392cad729694f3e7fdc36
Shereef Habib <shereef.habib@icubefarmhq.com>
2024-03-09 09:27:32 +0100

    staging poppins

--- frontend/src/components/LandingPage/heroSection.tsx
+++ frontend/src/components/LandingPage/heroSection.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, {ReactNode} from 'react'
 
-export const HeroTextAndCTA = () => {
+interface HeroTextAndCTAProps {
+    children: ReactNode;
+  }
+
+
+ const HeroTextAndCTA: React.FC<HeroTextAndCTAProps> = ({children}) => {
   return (
-    <div className="w-[90%] mt-24 min-h-10 bg-red-600"></div>
+    <div className="w-[90%] min-h-10 mt-[60px]">
+        {children}
+    </div>
   )
 }
+
+export const HeroText = () => {
+    return (
+        <h1 className="text-black w-[48%] text-[70px] font-bold leading-normal">
+            🌶️ Savor the Spice, Share the Smile! 😁
+        </h1>
+    )
+}
+
+
+export default HeroTextAndCTA
--- frontend/src/pages/landingPage.tsx
+++ frontend/src/pages/landingPage.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import AppSection from '../components/AppSection'
-import { NavigationBar } from '../components/Navigate'
-import { HeroTextAndCTA } from '../components/LandingPage/heroSection'
+import React from "react";
+import AppSection from "../components/AppSection";
+import { NavigationBar } from "../components/Navigate";
+import HeroTextAndCTA, {
+  HeroText,
+} from "../components/LandingPage/heroSection";
 
 export const LandingPage = () => {
   return (
     <AppSection>
       <NavigationBar />
-      <HeroTextAndCTA />
+      <HeroTextAndCTA>
+        <HeroText />
+      </HeroTextAndCTA>
     </AppSection>
-
-  )
-}
+  );
+};
