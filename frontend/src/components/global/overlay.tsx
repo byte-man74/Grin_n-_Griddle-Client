8b6ceb29260e11bd1e31a2834420311ca13f8b2b
Shereef Habib <sheryf.534@gmail.com>
2024-03-10 07:54:56 +0100

    staging overlay

---
+++ frontend/src/components/global/overlay.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const OverLay: React.FC = () => {
+    return (
+        <div className="w-full h-screen fixed top-0 bg-black"></div>
+      )
+}
--- frontend/src/pages/landingPage.tsx
+++ frontend/src/pages/landingPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import AppSection from "../components/AppSection";
 import { NavigationBar } from "../components/Navigate";
 import HeroTextAndCTA, {
@@ -6,10 +6,13 @@ import HeroTextAndCTA, {
   HeroText,
 } from "../components/LandingPage/heroSection";
 import { LandingPageImage } from "../components/LandingPage/landingPageImage";
+import { OverLay } from "../components/global/overlay";
 
 export const LandingPage = () => {
+//  const [modal, setModal] = useState(false)
   return (
     <AppSection>
+      <OverLay/>
       <NavigationBar />
       <HeroTextAndCTA>
         <HeroText />
