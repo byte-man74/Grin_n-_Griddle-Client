589c00f9cd60245c26f6f1e91ab270bde57fefc7
Shereef Habib <sheryf.534@gmail.com>
2024-03-10 09:00:17 +0100

    moved the modal to app section

---
+++ frontend/src/components/global/AppSection.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode, useState } from "react";
+import { OverLayState } from "./types";
+import SignUpComponent from "./AppSignUp";
+import { OverLay } from "./overlay";
+import LoginComponent from "./AppLogin";
+import { handleCloseModal } from "./utils/buttonAction";
+
+interface AppSectionProps {
+  children: ReactNode;
+}
+
+const AppSection: React.FC<AppSectionProps> = ({ children }) => {
+  const [activeState, setActiveState] = useState<OverLayState | null>(null);
+
+  const States: OverLayState[] = [
+    {
+      index: 1,
+      name: "SIGN UP",
+      component: <SignUpComponent setActiveState={setActiveState} />,
+    },
+    {
+      index: 2,
+      name: "LOGIN",
+      component: <LoginComponent setActiveState={setActiveState} />,
+    },
+  ];
+
+  const CloseModal = () => handleCloseModal(setActiveState);
+
+  const Modal = ({
+    activeState,
+  }: {
+    activeState: OverLayState | null;
+    onClose: () => void;
+  }) => {
+    return <OverLay>{activeState?.component}</OverLay>;
+  };
+
+  return (
+    <div className="relative flex max-w-[3000px] mx-auto flex-col items-center w-full min-h-screen pt-4 md:pt-0 bg-backgroundPrimary">
+      {activeState !== null && (
+        <Modal activeState={activeState} onClose={CloseModal} />
+      )}
+      {children}
+    </div>
+  );
+};
+
+export default AppSection;
--- frontend/src/pages/landingPage/index.tsx
+++ frontend/src/pages/landingPage/index.tsx
@@ -1,16 +1,11 @@
-import React, { useState } from "react";
-import AppSection from "../../components/AppSection";
+import React from "react";
+import AppSection from "../../components/global/AppSection";
 import { NavigationBar } from "../../components/Navigate";
 import HeroTextAndCTA, {
   HeroCTA,
   HeroText,
 } from "../../components/LandingPage/heroSection";
 import { LandingPageImage } from "../../components/LandingPage/landingPageImage";
-import { OverLay } from "../../components/global/overlay";
-import { OverLayState } from "../../components/global/types";
-import SignUpComponent from "../../components/global/AppSignUp";
-import LoginComponent from "../../components/global/AppLogin";
-import { handleCloseModal } from "./utils/buttonAction";
 
 
 
@@ -22,44 +17,10 @@ import { handleCloseModal } from "./utils/buttonAction";
 
 
 export const LandingPage = () => {
-  const [activeState, setActiveState] = useState<OverLayState | null>(null);
-
-  const States: OverLayState[] = [
-    {
-      index: 1,
-      name: "SIGN UP",
-      component: <SignUpComponent setActiveState={setActiveState}/>,
-    },
-    {
-      index: 2,
-      name: "LOGIN",
-      component: <LoginComponent setActiveState={setActiveState}/>,
-    },
-  ];
-  
-
-
-  const CloseModal = () => handleCloseModal(setActiveState);
-
-  
-  const Modal = ({
-    activeState,
-  }: {
-    activeState: OverLayState | null;
-    onClose: () => void;
-  }) => {
-    return (
-      <OverLay>
-        {activeState?.component}
-      </OverLay>
-    );
-  };
-
 
 
   return (
     <AppSection>
-      {activeState !== null && <Modal activeState={activeState} onClose={CloseModal} />}
       <NavigationBar />
       <HeroTextAndCTA>
         <HeroText />
