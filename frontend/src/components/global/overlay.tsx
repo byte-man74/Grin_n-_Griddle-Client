bfbcde033a01c640dab4926605851bfab3d4d52b
Shereef Habib <sheryf.534@gmail.com>
2024-03-10 08:34:45 +0100

    staging the modal

--- frontend/src/components/global/overlay.tsx
+++ frontend/src/components/global/overlay.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { ReactNode }  from 'react'
 
-export const OverLay: React.FC = () => {
+
+interface OverlayProps {
+    children: ReactNode;
+  }
+
+
+
+export const OverLay: React.FC<OverlayProps> = ({children}) => {
     return (
-        <div className="w-full h-screen fixed top-0 bg-black"></div>
+        <div className="fixed top-0 z-10 flex justify-center w-full h-screen py-10 bg-[#00000080]">
+            {children}
+        </div>
       )
 }
--- frontend/src/pages/landingPage.tsx
+++ frontend/src/pages/landingPage.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { ReactNode, useState } from "react";
 import AppSection from "../components/AppSection";
 import { NavigationBar } from "../components/Navigate";
 import HeroTextAndCTA, {
@@ -8,11 +8,67 @@ import HeroTextAndCTA, {
 import { LandingPageImage } from "../components/LandingPage/landingPageImage";
 import { OverLay } from "../components/global/overlay";
 
+
+
+
+
+
+const SignUpComponent = () => {
+  return <div className="w-[300px] h-[200px] bg-white text-black">Sign Up Content</div>;
+};
+
+const LoginComponent = () => {
+  return <div>Login Content</div>;
+};
+
+
+interface OverLayState {
+  index: number;
+  name: string;
+  component: ReactNode;
+}
+
+const States: OverLayState[] = [
+  {
+    index: 1,
+    name: "SIGN UP",
+    component: <SignUpComponent />,
+  },
+  {
+    index: 2,
+    name: "LOGIN",
+    component: <LoginComponent />,
+  },
+];
+
 export const LandingPage = () => {
-//  const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(true);
+  const [activeState, setActiveState] = useState<OverLayState | null>(States[0]);
+
+
+
+  const handleCloseModal = () => {
+    setModal(false);
+    setActiveState(null);
+  };
+  const Modal = ({
+    activeState,
+    onClose,
+  }: {
+    activeState: OverLayState | null;
+    onClose: () => void;
+  }) => {
+    return (
+      <OverLay>
+        {activeState?.component}
+        <button onClick={onClose}>Close Modal</button>
+      </OverLay>
+    );
+  };
+
   return (
     <AppSection>
-      <OverLay/>
+      {modal && <Modal activeState={activeState} onClose={handleCloseModal} />}
       <NavigationBar />
       <HeroTextAndCTA>
         <HeroText />
