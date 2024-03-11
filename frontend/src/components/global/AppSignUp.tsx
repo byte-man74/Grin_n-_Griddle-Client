ed500259ff29ad30337c94e59e9d4e69fcddfa03
Shereef Habib <sheryf.534@gmail.com>
2024-03-11 08:58:35 +0100

    sign up form complete

--- frontend/src/components/global/AppSignUp.tsx
+++ frontend/src/components/global/AppSignUp.tsx
@@ -9,7 +9,6 @@ interface SignUpProps {
 const SignUpComponent: React.FC<SignUpProps> = ({ setActiveState }) => {
   return (
     <div className="w-[550px] min-h-[200px] rounded-md p-4 bg-white text-primaryDark">
-
       {/* modal header */}
       <div className="flex items-center justify-between ">
         <div className="flex flex-col gap-[4px]">
@@ -21,17 +20,36 @@ const SignUpComponent: React.FC<SignUpProps> = ({ setActiveState }) => {
         <Cancel setActiveState={setActiveState} />
       </div>
 
-
       {/* modal form content */}
       <div className="w-full mt-10 min-h-5">
-      <input type="text" className="w-full h-[55px] px-2 mb-8 bg-white border-b border-[rgba(40, 40, 40, 0.26)] border-solid outline-none" placeholder="Enter phone number" />
-      <input type="password" className="w-full mb-8 h-[55px] px-2 bg-white border-b border-[rgba(40, 40, 40, 0.26)] border-solid outline-none" placeholder="Enter password" />
-      <input type="password" className="w-full h-[55px] mb-2 px-2 bg-white border-b border-[rgba(40, 40, 40, 0.26)] border-solid outline-none" placeholder="Confirm password" />
-      <p className="text-[14px] text-primaryDark mb-14">
-            Already have an account? <span className="font-bold text-[#FF6F61]">Login</span>
-          </p>
+        <input
+          type="text"
+          className="w-full h-[55px] px-2 mb-8 bg-white border-b border-[rgba(40, 40, 40, 0.26)] border-solid outline-none"
+          placeholder="Enter phone number"
+        />
+        <input
+          type="password"
+          className="w-full mb-8 h-[55px] px-2 bg-white border-b border-[rgba(40, 40, 40, 0.26)] border-solid outline-none"
+          placeholder="Enter password"
+        />
+        <input
+          type="password"
+          className="w-full h-[55px] mb-2 px-2 bg-white border-b border-[rgba(40, 40, 40, 0.26)] border-solid outline-none"
+          placeholder="Confirm password"
+        />
+        <p className="text-[14px] text-primaryDark mb-14">
+          Already have an account?{" "}
+          <motion.span
+            whileHover={{ scale: 0.9 }}
+            className="font-bold text-[#FF6F61] cursor-pointer"
+          >
+            Login
+          </motion.span>
+        </p>
 
-      <GradientButton icon={<FrontIcon />} width="100%" height="55px">Continue</GradientButton>
+        <GradientButton icon={<FrontIcon />} width="100%" height="55px">
+          Continue
+        </GradientButton>
       </div>
     </div>
   );
@@ -39,7 +57,12 @@ const SignUpComponent: React.FC<SignUpProps> = ({ setActiveState }) => {
 
 const Cancel: React.FC<SignUpProps> = ({ setActiveState }) => {
   return (
-    <motion.div whileHover={{ scale: 0.75, backgroundColor: "#FF6F61" }} whileTap={{ scale: 0.94 }} className="cursor-pointer p-[2px] rounded-full" onClick={() => setActiveState(null)}>
+    <motion.div
+      whileHover={{ scale: 0.75, backgroundColor: "#FF6F61" }}
+      whileTap={{ scale: 0.94 }}
+      className="cursor-pointer p-[2px] rounded-full"
+      onClick={() => setActiveState(null)}
+    >
       <CancelSvg />
     </motion.div>
   );
