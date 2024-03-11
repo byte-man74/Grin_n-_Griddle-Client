c68e30cfce7a5713c179ec4c483cb7853831395c
Shereef Habib <sheryf.534@gmail.com>
2024-03-11 07:51:16 +0100

    staging sign up modal content

--- frontend/src/components/global/AppSignUp.tsx
+++ frontend/src/components/global/AppSignUp.tsx
@@ -5,8 +5,10 @@ interface SignUpProps {
 
 const SignUpComponent: React.FC<SignUpProps> = () => {
   return (
-    <div className="w-[550px] min-h-[200px] bg-white text-black">
-      Sign Up Content
+    <div className="w-[550px] min-h-[200px] rounded-md py-3 px-2 bg-white text-black">
+      <div className="flex flex-col gap-3">
+        <h4 className="font-bold text-[25px]">Get started</h4>
+      </div>
     </div>
   );
 };
